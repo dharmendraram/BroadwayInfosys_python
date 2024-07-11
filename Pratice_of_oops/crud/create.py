a7edee0da9eb32fd7ab514d34fa445b02d85bb9f
dharmendraram <dharmendraram7852@gmail.com>
2024-07-11 16:18:09 +0545

    crud pratice update

--- Pratice_of_oops/crud/create.py
+++ Pratice_of_oops/crud/create.py
@@ -1,34 +1,34 @@
+
 import json
 import os
 
 filename = "students.json"
 
-
 def create_student():
-
     id = input("Enter student id:")
     name = input("Enter student name:")
     age = input("Enter student age:")
-    address = input("Enter student address:")
+    address= input("Enter student address:")
 
-    student = {"id": id, "name": name, "age": age, "address": address}
-
+    student = {"id":id, "name":name, "age":age, "address":address}
     try:
         if not os.path.exists(filename):
             with open(filename, "w") as fp:
-
-                # json.dump(student,fp,indent=2)
                 to_dump = json.dumps([student], indent=2)
                 fp.write(to_dump)
+
         else:
             with open(filename, "r+") as fp:
-                students = json.loads(fp.read())  # [{},{}]
+                students = json.loads(fp.read())
                 students.append(student)
                 fp.seek(0)
                 fp.write(json.dumps(students, indent=2))
-        print("Student Added Successfully !")
+            print("Students Added Successfully !")
     except IOError as e:
-        print(f"Error: {e}")
-        
-    choice = input("Do you want to continue? (y/n):")
-    return True if choice.lower() == "y" else False
+        print(f"Error:{e}")
+
+    choice = input("Do yo want to continue? (y/n):")
+    return True if choice.strip().lower() == "y" else False
+
+
+
